f5e39e58a20a5ac6bdffcf063faf36e26bcf50c3
Setinais13 <vynny.cg@gmail.com>
2017-06-26 20:27:09 -0300

    Iniciando App Espanglish 2017

--- app/src/main/java/br/edu/vinnicyus/espanglish/Model/Jurado.java
+++ app/src/main/java/br/edu/vinnicyus/espanglish/Model/Jurado.java
@@ -1,8 +1,13 @@
 package br.edu.vinnicyus.espanglish.Model;
 
+import com.activeandroid.Model;
+import com.activeandroid.annotation.Table;
+
 /**
  * Created by Vinnicyus on 26/06/2017.
  */
 
-public class Jurado {
+@Table (name = "jurados")
+public class Jurado extends Model{
+
 }
